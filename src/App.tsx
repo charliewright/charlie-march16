020bbce9cced4798cf8061ff66f76c2ce45b79bb
Charles Wright <wrightcharlesh@gmail.com>
2021-03-14 20:09:27 -0700

    initial commit for mmit for create react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Order book app</h1>
+    </div>
+  );
+}
+
+export default App;
